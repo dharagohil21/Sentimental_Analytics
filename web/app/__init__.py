92c1152c477c6e2a5ba50d0cd852fbc064ed1de9
Daniel Penalo <edaniel.penalo@gmail.com>
2021-07-29 18:36:19 -0300

    Done

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect
 from app.mod_home.controllers import mod_home as home_module
 from app.mod_auth.controllers import mod_auth as auth_module
+from app.mod_results.controllers import mod_results as results_module
 from app.mod_sentimentrequest.controllers import (
         mod_sentimentrequest as sentimentrequest_module
     )
@@ -25,6 +26,7 @@ def not_found(error):
 app.register_blueprint(home_module)
 app.register_blueprint(auth_module)
 app.register_blueprint(sentimentrequest_module)
+app.register_blueprint(results_module)
 
 if __name__ == '__main__':
     with app.app_context():
